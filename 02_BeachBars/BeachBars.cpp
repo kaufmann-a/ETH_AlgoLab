00497b1b0d3fdbf576b739373fcc20dbe43dee2e
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-09-29 12:12:53 +0200

    Problem of the week

--- 02_BeachBars/BeachBars.cpp
+++ 02_BeachBars/BeachBars.cpp
@@ -1,8 +1,64 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
+#include<limits>
+#include<set>
+#include<math>
 
 void testcase(){
+	int n; std::cin >> n;
+
+	std::vector<bool> beach(1000000*2+1, false) ;
+	for (int i = 0; i < n; i++){
+		int pos; std::cin >> pos;
+		beach[pos+100000] = true;
+	}
+
+	std::set<int> best;
+
+	int curHighestAmount = 0;
+	int curShortestDistance = std::numeric_limits<int>::max();
+	//Build sum for first 100 parasols
+	for (int i = 0; i <= 100; i++){
+		if (beach[i] == true){
+			curHighestAmount++;
+			curShortestDistance = i;
+		}
+	}
+	best.insert(0);
+
+	int curAmount = curHighestAmount;
+	int curDistance = curShortestDistance;
+	//Iterate through beach
+	for (int i = 1; i <= 2000000; i++){
+		//check if at left border of beach
+		if (i < 100){
+			if (beach[i+100] == true){
+				curAmount++;
+				curDistance = 100;
+				if(curAmount > curHighestAmount){
+					best.clear();
+					best.insert(i);
+					curShortestDistance = 100;
+				}
+			} else {
+				curDistance--;
+				best.clear();
+				best.insert(i);
+			}
+
+		} else if (i+100 > 2000000){
+			if (beach[i-101] == false){
+				if (curDistance < 0){
+					curDistance--;
+				} else {
+					curDistance++;
+				}
+				if (std::abs(curDistance))
+
+			}
+		}
+	}
 
 }
 
---
+++ 03_Russia/Russia.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<set>
+
+int solve(std::vector<std::vector<std::vector<int>>> &dp, std::vector<int> &coins, int left, int right, int currPers, int k ,int totP){
+
+	if (left > right){
+		return 0;
+	}
+
+
+	if (left == right){
+		if (currPers == k){
+            return coins[left];
+        } else {
+            return 0;
+        }
+	}
+
+    int dpPersonIndx = 0;
+    if (currPers == k){
+        dpPersonIndx = 1;
+    }
+
+    if (dp[dpPersonIndx][left][right] != -1){
+        return dp[dpPersonIndx][left][right];
+    }
+
+    int res = 0;
+    int nextPerson = (currPers + 1) % totP;
+    if (currPers != k){
+        res = std::min(solve(dp, coins, left+1, right, nextPerson, k, totP), solve(dp, coins, left, right-1, nextPerson, k, totP));
+    } else {
+        res = std::max(coins[left] + solve(dp, coins, left+1, right, nextPerson, k, totP), coins[right] + solve(dp, coins, left, right-1, nextPerson, k, totP));
+    }
+    dp[dpPersonIndx][left][right] = res;
+    return res;
+
+}
+
+
+void testcase(){
+	int n, m, k; std::cin >> n; std::cin >> m; std::cin >> k;
+	std::vector<int> coins(n);
+	std::vector<std::vector<std::vector<int>>> dp(2, std::vector<std::vector<int>>(n, std::vector<int>(n, -1)));
+
+	for (int i = 0; i < n; i++){
+		std::cin >> coins.at(i);
+	}
+
+	int res = solve(dp, coins, 0, n-1, 0, k, m);
+	std::cout << res << std::endl;
+
+}
+
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+
+	std::fstream in("./testsets/sample.in");
+	std::cin.rdbuf(in.rdbuf());
+
+	int t;std::cin >> t;
+
+	for (int i = 0; i < t; i++){
+		testcase();
+	}
+
+
+
+	return 0;
+}
