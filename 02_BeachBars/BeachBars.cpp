3f290a37877add89d8d8016bb4d8bf6f396bd117
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-09-29 20:13:14 +0200

    Programmed BeachBars again, this time with sliding window approach

--- 02_BeachBars/BeachBars.cpp
+++ 02_BeachBars/BeachBars.cpp
@@ -5,110 +5,78 @@
 #include<set>
 //#include<math>
 
-int BEACHSIDE = 1000000;
-int BEACHLENGTH = 2*BEACHSIDE + 1;
 
 void testcase(){
 	int n; std::cin >> n;
 
-	std::vector<bool> beach(BEACHLENGTH, false) ;
-	//Fill beach with parasoles
-	for (int i = 0; i < n; i++){
-		int pos; std::cin >> pos;
-		beach[pos+BEACHSIDE] = true;
+	std::set<int> coordinates;
+	for (int i = 0; i < n; ++i){
+		int val; std::cin >> val;
+		coordinates.insert(val);
 	}
 
-	std::set<int> best;
+	std::set<int> optimalPos;
+	int maxNrOfParasols = 0;
+	int minDistance = std::numeric_limits<int>::max();
 
-	int curHighestAmount = 0;
-	int curShortestDistance = std::numeric_limits<int>::max();
-	//Build sum for first 100 parasols
-	for (int i = 0; i <= 100; i++){
-		if (beach[i] == true){
-			curHighestAmount++;
-			curShortestDistance = i;
+	int nrOfParasols = 0;
+	int intervalSize = 0;
+
+	std::set<int>::iterator left = coordinates.begin();
+	std::set<int>::iterator right = coordinates.begin();
+	std::set<int>::iterator rightOld = coordinates.begin();
+
+	do {
+		do {
+			rightOld = right;
+			nrOfParasols++;
+			right++;
+			intervalSize = *right - *left;
+
+		} while (intervalSize <= 200 && right != coordinates.end());
+
+		//Calculate
+		int midPosition = (*rightOld-*left)/2 + *left;
+		int curMinDistance = std::max(*rightOld - midPosition, midPosition - *left);
+
+		//Clear optimalPos set if new best location found
+		if (nrOfParasols > maxNrOfParasols || (nrOfParasols == maxNrOfParasols && curMinDistance < minDistance)){
+			maxNrOfParasols = nrOfParasols;
+			minDistance = curMinDistance;
+			optimalPos.clear();
 		}
-	}
-	best.insert(0);
+		//Add to set if equal or better position found
+		if (nrOfParasols >= maxNrOfParasols && curMinDistance <= minDistance){
+			optimalPos.insert(midPosition);
 
-	int curAmount = curHighestAmount;
-	int curDistance = curShortestDistance;
-	//Iterate through beach
-	for (int i = 1; i <= 2*BEACHSIDE; i++){
-		//check if at left border of beach
-		if (i <= 100){
-			//Case new parasol found, Amount must be larger as all previous parasoles kept
-			if (beach.at(i+100) == true){
-				curAmount++;
-				curHighestAmount = curAmount;
-				curDistance = 100;
-				curShortestDistance = curDistance;
-				best.clear();
-				best.insert(i);
-			} else {
-				//Just Update distance
-				curDistance--;
-				if (std::abs(curDistance) < std::abs(curShortestDistance)){
-					curShortestDistance = curDistance;
-					best.clear();
-					best.insert(i);
-				} else if (std::abs(curDistance) == std::abs(curShortestDistance)){
-					best.insert(i);
-				}
-			}
-		} else if (i <= 2*BEACHSIDE-100){
-			//Add remove potential new parasoles
-			if (beach.at(i+100) == true){
-				curAmount++;
-			}
-			if (beach.at(i-101) == true){
-				curAmount--;
-			}
-
-			if (curAmount > curHighestAmount){
-				curHighestAmount = curAmount;
-				curDistance = 100;
-				curShortestDistance = curDistance;
-				best.clear();
-				best.insert(i);
-			} else if (curAmount == curHighestAmount){
-				curDistance--;
-				if(std::abs(curDistance) < std::abs(curShortestDistance)){
-					curShortestDistance = curDistance;
-					best.clear();
-					best.insert(i);
-				} else if (std::abs(curDistance) == std::abs(curShortestDistance)){
-					best.insert(i);
-				}
-			}
-		} else if (i <= 2*BEACHSIDE){
-			if (beach.at(i-101) == true){
-				break;
-			} else {
-				curDistance--;
-				if(std::abs(curDistance) < std::abs(curShortestDistance)){
-					curShortestDistance = curDistance;
-					best.clear();
-					best.insert(i);
-				} else if (std::abs(curDistance) == std::abs(curShortestDistance)){
-					best.insert(i);
-				}
+			//Check if interval was odd number
+			if ((*rightOld-*left) % 2 == 1){
+				optimalPos.insert(midPosition+1);
 			}
 		}
-	}
 
-	std::cout << curHighestAmount << " " << curShortestDistance << std::endl;
-	for (std::set<int>::iterator it = best.begin(); it!= best.end(); it++){
+		//Iterate left marker until interval size is below 200 again
+		while (intervalSize > 200 && left != right){
+			left++;
+			nrOfParasols--;
+			intervalSize = *right - *left;
+		} 
+	} while (right != coordinates.end());
+	
+	std::cout << maxNrOfParasols << " " << minDistance << std::endl;
+	for (std::set<int>::iterator it = optimalPos.begin(); it != optimalPos.end(); it++){
 		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
+
+
 }
 
 
 int main(){
 	std::ios_base::sync_with_stdio(false);
 
-	std::fstream in("./testsets/sample.in");
+	std::fstream in("./testsets/test1.in");
 	std::cin.rdbuf(in.rdbuf());
 
 	int t;std::cin >> t;
