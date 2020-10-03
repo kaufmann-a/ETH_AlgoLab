1ef383f58ca2f5885fe3e33435c3016d9e02884b
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-10-03 11:28:43 +0200

    Init for 03_Hit task

---
+++ 03_Hit/hit.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+
+void testcase(){
+    std::cout << std::endl;
+}
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+
+    std::fstream in("./testsets/sample.in");
+    std::cin.rdbuf(in.rdbuf());
+
+    int t; std::cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        testcase();
+    }
+    
+}
