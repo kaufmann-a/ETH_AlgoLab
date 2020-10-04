8061127a2de2683e9be491c2f9162afb875caf61
algolab <algolab@judge-algolab.inf.ethz.ch>
2020-10-04 14:45:03 +0200

    Changed solution to version which does not calculate segment-length, but just cut the ray

--- 03_FirstHit/src/algorithm.cpp
+++ 03_FirstHit/src/algorithm.cpp
@@ -27,16 +27,15 @@ void testcase(const int n) {
 	long y; std::cin >> y;
 	long a; std::cin >> a;
 	long b; std::cin >> b;
-	IK::Point_2 p(x, y), q(a, b);
-	IK::Ray_2 ray(p, q);
-	EK::Point_2 ep(p.x(), p.y()), eq(q.x(), q.y());
-	EK::Ray_2 eray(ep, eq);
-
+	EK::Point_2 ep(x, y), eq(a, b);
+	EK::Ray_2 ray(ep, eq);
 
 	EK::FT bestdist = std::numeric_limits<long>::max();
+	EK::Segment_2 bestseg;
 	EK::Point_2 bestPoint(0, 0);
 	bestdist = bestdist * bestdist;
-	bool doIntersect = false;
+
+	bool intersectionAnywhere = false;
 	//Create segments and check for intersect
 	for (int i = 0; i < n; i++){
 
@@ -44,41 +43,43 @@ void testcase(const int n) {
 		long s; std::cin >> s;
 		long t; std::cin >> t;
 		long u; std::cin >> u;
-		IK::Point_2 segS(r, s), segT(t, u);
-		IK::Segment_2 seg(segS, segT);
+		EK::Point_2 segS(r, s), segT(t, u);
+		EK::Segment_2 seg(segS, segT);
+		bool doIntersect = false;
+		if (!intersectionAnywhere){
+			doIntersect = do_intersect(ray, seg);
+		} else {
+			doIntersect = do_intersect(bestseg, seg);
+		}
 
-		if (CGAL::do_intersect(ray, seg)){
-			EK::Point_2 esegS(segS.x(), segS.y()), esegT(segT.x(), segT.y());
-			EK::Segment_2 eseg(esegS, esegT);
-			EK::FT dist = 0;
-			auto o = CGAL::intersection(eseg, eray);
+		if (doIntersect){
+			intersectionAnywhere = true;
+			auto o = CGAL::intersection(seg, ray);
 			if (const EK::Point_2* op = boost::get<EK::Point_2>(&*o)){
-				EK::FT dist = CGAL::squared_distance(*op, ep);
-				if (dist < bestdist){
-					bestdist = dist;
-					bestPoint = *op;
-				}
+				// EK::FT dist = CGAL::squared_distance(*op, ep);
+				// if (dist < bestdist){
+				// 	bestdist = dist;
+				// 	bestPoint = *op;
+				// }
+				bestseg = EK::Segment_2(ep, *op);
 			} else if (const EK::Segment_2* op = boost::get<EK::Segment_2>(&*o)) {
+				
 				EK::FT dist_source = CGAL::squared_distance(op->source(), ep);
 				EK::FT dist_target = CGAL::squared_distance(op->target(), ep);
-				if (dist_source < bestdist){
+				if (dist_source < dist_target){
 					bestPoint = op->source();
-					bestdist = dist_source;
-				}
-				if (dist_target < bestdist){
-					bestPoint = op->target();
-					bestdist = dist_target;
+					bestseg = EK::Segment_2(ep, op->source());
+				} else {
+					bestseg = EK::Segment_2(ep, op->target());
 				}
 			} else {
 				std::cout << "What the heck is happening here" << std::endl;
 			}
-			doIntersect = true;
 		}
-
 	}
 
-	if (doIntersect){
-		std::cout << (long) floor_to_double(bestPoint.x()) << " " << (long) floor_to_double(bestPoint.y()) << std::endl;
+	if (intersectionAnywhere){
+		std::cout << (long) floor_to_double(bestseg.target().x()) << " " << (long) floor_to_double(bestseg.target().y()) << std::endl;
 	} else {
 		std::cout << "no" << std::endl;
 	}
