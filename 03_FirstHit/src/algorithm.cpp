76a514f37992fd1b7ac73ea0ba8dfb121a1c8abd
algolab <algolab@judge-algolab.inf.ethz.ch>
2020-10-04 14:07:30 +0200

    First running version of first hit

--- 03_FirstHit/src/algorithm.cpp
+++ 03_FirstHit/src/algorithm.cpp
@@ -1,3 +1,4 @@
+
 #include <limits>
 #include <iostream>
 #include <iomanip>
@@ -21,8 +22,6 @@ double floor_to_double(const EK::FT& x){
 }
 
 void testcase(const int n) {
-	bool doesIntersect = false;
-
 	//Create ray
 	long x; std::cin >> x;
 	long y; std::cin >> y;
@@ -35,6 +34,8 @@ void testcase(const int n) {
 
 
 	EK::FT bestdist = std::numeric_limits<long>::max();
+	EK::Point_2 bestPoint(0, 0);
+	bestdist = bestdist * bestdist;
 	bool doIntersect = false;
 	//Create segments and check for intersect
 	for (int i = 0; i < n; i++){
@@ -46,37 +47,47 @@ void testcase(const int n) {
 		IK::Point_2 segS(r, s), segT(t, u);
 		IK::Segment_2 seg(segS, segT);
 
-
-		bestdist = bestdist * bestdist;
 		if (CGAL::do_intersect(ray, seg)){
 			EK::Point_2 esegS(segS.x(), segS.y()), esegT(segT.x(), segT.y());
 			EK::Segment_2 eseg(esegS, esegT);
 			EK::FT dist = 0;
 			auto o = CGAL::intersection(eseg, eray);
 			if (const EK::Point_2* op = boost::get<EK::Point_2>(&*o)){
-				dist = CGAL::squared_distance(*op, ep);
+				EK::FT dist = CGAL::squared_distance(*op, ep);
+				if (dist < bestdist){
+					bestdist = dist;
+					bestPoint = *op;
+				}
+			} else if (const EK::Segment_2* op = boost::get<EK::Segment_2>(&*o)) {
+				EK::FT dist_source = CGAL::squared_distance(op->source(), ep);
+				EK::FT dist_target = CGAL::squared_distance(op->target(), ep);
+				if (dist_source < bestdist){
+					bestPoint = op->source();
+					bestdist = dist_source;
+				}
+				if (dist_target < bestdist){
+					bestPoint = op->target();
+					bestdist = dist_target;
+				}
 			} else {
 				std::cout << "What the heck is happening here" << std::endl;
-			}
-			if (dist < bestdist){
-				bestdist = dist;
 			}
 			doIntersect = true;
 		}
 
 	}
 
-	// if (doIntersect){
-	// 	std::cout << floor_to_double(bestdist.x()) << << floor_to_double(bestdist.y()) std::endl;
-	// } else {
-	// 	std::cout << "no" << std::endl;
-	// }
+	if (doIntersect){
+		std::cout << (long) floor_to_double(bestPoint.x()) << " " << (long) floor_to_double(bestPoint.y()) << std::endl;
+	} else {
+		std::cout << "no" << std::endl;
+	}
 }
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
 
-	std::fstream in("./testsets/sample.in");
+	std::fstream in("./testsets/test.in");
 	std::cin.rdbuf(in.rdbuf());
 
 	int t; std::cin >> t;
