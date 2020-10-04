6b5066c3bcbca224305b96469b9ecf01c06735fa
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-10-04 23:20:23 +0200

    New solution, but to slow

--- 03_HikingMaps/src/algorithm.cpp
+++ 03_HikingMaps/src/algorithm.cpp
@@ -6,9 +6,9 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel IK;
+typedef CGAL::Exact_predicates_exact_constructions_kernel IK;
 
 void testcase() {
 	int m; std::cin >> m;
@@ -46,23 +46,27 @@ void testcase() {
 			}
 		}
 		IK::Triangle_2 map(corners[0], corners[1], corners[2]);
-		std::cout << "corn1: " << corners[0].x() << " " << corners[0].y() << "corn2: " << corners[1].x() << " " << corners[1].y() << "corn3: " << corners[2].x() << " " << corners[2].y() <<  std::endl;
+		//std::cout << "corn1: " << corners[0].x() << " " << corners[0].y() << "corn2: " << corners[1].x() << " " << corners[1].y() << "corn3: " << corners[2].x() << " " << corners[2].y() <<  std::endl;
 		mapParts[i] = map;
 	}
 
 	//Check which maps contain which paths
 	std::vector<std::vector<int>> map_nr_contains_paths_nr(n);
 	for (int i = 0; i < n; i++){ //Loop over every map part
-		for (int j = 0; j < m-2; j++){
-			auto o = CGAL::intersection(mapParts[i], segments[j]);
-			if (const IK::Segment_2* op = boost::get<IK::Segment_2>(&*o)){
-				std::cout << "source_op: " << op->source().x() << " " << op->source().y() << " target_op: " << op->target().x() << " " << op->target().y() << std::endl;
-				std::cout << "source_seg: " << segments[j].source().x() << " " << segments[j].source().y() << " target_seg: " << segments[j].target().x() << " " << segments[j].target().y() << std::endl;
-				
-				if (op->source() == segments[j].source() && op->target() == segments[j].target()){
-					map_nr_contains_paths_nr[i].push_back(j);
-				}
+		for (int j = 0; j < m-1; j++){
+			if (!mapParts[i].has_on_unbounded_side(segments[j].source()) && !mapParts[i].has_on_unbounded_side(segments[j].target())){
+        		map_nr_contains_paths_nr[i].push_back(j);
+				//std::cout << "lsdf " << j << std::endl;
 			}
+			// auto o = CGAL::intersection(mapParts[i], segments[j]);
+			// if (const IK::Segment_2* op = boost::get<IK::Segment_2>(&*o)){
+			// 	//std::cout << "source_op: " << op->source().x() << " " << op->source().y() << " target_op: " << op->target().x() << " " << op->target().y() << std::endl;
+			// 	//std::cout << "source_seg: " << segments[j].source().x() << " " << segments[j].source().y() << " target_seg: " << segments[j].target().x() << " " << segments[j].target().y() << std::endl;
+				
+			// 	if (op->source() == segments[j].source() && op->target() == segments[j].target()){
+			// 		map_nr_contains_paths_nr[i].push_back(j);
+			// 	}
+			// }
 		}
 	}
 
@@ -70,24 +74,24 @@ void testcase() {
 	int leftIdx = 0; int rightIdx = 0;
 	int best_e = std::numeric_limits<int>::max(); int best_b = 0;
 	int pathLength = 0;
-	while (rightIdx < n && pathLength < m) {
+	std::vector<int> curPathPartsCovered(m-1);
+	while (rightIdx < n) {
 		//Fill up vector curPathPartsCovered, which tracks the segments currently covered by maps
-		std::vector<int> curPathPartsCovered(m-1);
-		for (uint i = 0; i < map_nr_contains_paths_nr[rightIdx].size(); i++){
-			if (curPathPartsCovered[map_nr_contains_paths_nr[rightIdx][i]] == 0){
-				pathLength++;
+		while (rightIdx < n && pathLength < m-1){
+			for (uint i = 0; i < map_nr_contains_paths_nr[rightIdx].size(); i++){
+				if (curPathPartsCovered[map_nr_contains_paths_nr[rightIdx][i]] == 0){
+					pathLength++;
+				}
+				curPathPartsCovered[map_nr_contains_paths_nr[rightIdx][i]] += 1;
 			}
-			curPathPartsCovered[map_nr_contains_paths_nr[rightIdx][i]] += 1;
+			rightIdx++;
 		}
-		
-		if (pathLength == m-1){
+
+		while (pathLength == m-1 && leftIdx < rightIdx){
 			if (rightIdx - leftIdx < best_e - best_b){
 				best_e = rightIdx;
 				best_b = leftIdx;
 			}
-		}
-
-		while (pathLength == m-1 && leftIdx <= rightIdx){
 			for (uint i = 0; i < map_nr_contains_paths_nr[leftIdx].size(); i++){
 				curPathPartsCovered[map_nr_contains_paths_nr[leftIdx][i]] -= 1;
 				if (curPathPartsCovered[map_nr_contains_paths_nr[leftIdx][i]] == 0){
@@ -96,7 +100,6 @@ void testcase() {
 			}
 			leftIdx++;
 		}
-		rightIdx++;
 	}
 	int cost = best_e - best_b;
 	std::cout << cost << std::endl;
@@ -104,7 +107,7 @@ void testcase() {
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
-	std::fstream in("./testsets/sample.in");
+	std::fstream in("./testsets/test1.in");
 	std::cin.rdbuf(in.rdbuf());
 
 	int t;
