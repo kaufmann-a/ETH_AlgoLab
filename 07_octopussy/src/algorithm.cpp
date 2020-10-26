6cbc8f49bd52911cd61f6fe226aff9d70be2dc02
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-10-26 17:18:22 +0100

    Cleanup

--- 07_octopussy/src/algorithm.cpp
+++ 07_octopussy/src/algorithm.cpp
@@ -8,20 +8,12 @@
 #include <fstream>
 #include <math.h>
 
-
-struct Node {
-	int time, tamortized, nodeNr;
-
-	Node(int t, int ta, int nr) : time(t), tamortized(ta), nodeNr(nr){}
-};
-
-bool sortBy(Node &n1, Node &n2){
-	return n1.time < n2.time;
+bool sortBy(std::pair<int, int> &n1, std::pair<int, int> &n2){
+	return n1.second < n2.second;
 	
 }
 
-
-void processSubTreeNodes(const std::vector<Node> &nodes, std::vector<bool> &nodeProcessed, std::vector<Node> &order, int nodeNr, int n){
+void processSubTreeNodes(const std::vector<std::pair<int, int>> &nodes, std::vector<bool> &nodeProcessed, std::vector<std::pair<int, int>> &order, int nodeNr, int n){
 	if (nodeNr > n-1){
 		return;
 	}
@@ -34,41 +26,40 @@ void processSubTreeNodes(const std::vector<Node> &nodes, std::vector<bool> &node
 	}
 }
 
-
 void testcase() {
 	int n; std::cin >> n;
 	int nrLevels = std::log2(n+1);
 	int counter = 0;
-	std::vector<Node> nodes;
+	std::vector<std::pair<int, int>> nodes;
 	for (int i = 0; i < nrLevels; i++){
 		int curLevel = std::pow(2, i);
 		int nrNodesInSubtree = std::pow(2, nrLevels- i) - 2;
 		for (int j = 0; j < curLevel; j++){
 			int temp; std::cin >> temp;
-			nodes.push_back(Node(temp, temp-nrNodesInSubtree, counter));
+			nodes.push_back(std::make_pair(counter, temp));
 			counter++;
 		}
 	}
 
-	std::vector<Node> node_copy;
+	std::vector<std::pair<int, int>> node_copy;
 	for (int i = 0; i < n; i++){
 		node_copy.push_back(nodes[i]);
 	}
 	std::sort(node_copy.begin(), node_copy.end(), sortBy);
 
-	std::vector<Node> order;
+	std::vector<std::pair<int, int>> order;
 	std::vector<bool> nodeProcessed(n, false);
-	for (std::vector<Node>::iterator it = node_copy.begin(); it != node_copy.end(); it++){
-		if (!nodeProcessed[it->nodeNr]){
-			processSubTreeNodes(nodes, nodeProcessed, order, nodes[it->nodeNr].nodeNr, n);
+	for (std::vector<std::pair<int, int>>::iterator it = node_copy.begin(); it != node_copy.end(); it++){
+		if (!nodeProcessed[it->first]){
+			processSubTreeNodes(nodes, nodeProcessed, order, nodes[it->first].first, n);
 			//order.push_back(nodes[i]);
 		}
 	}
 
-	//NOw iterate thorugh order array and check if explosion somewhere
+	//Now iterate thorugh order array and check if explosion somewhere
 	bool explosion = false;
 	for (int i = 0; i < n; i++){
-		if (order[i].time <= i){
+		if (order[i].second <= i){
 			explosion = true;
 			break;
 		}
@@ -79,7 +70,6 @@ void testcase() {
 	} else {
 		std::cout << "yes" << std::endl;
 	}
-
 	return;
 }
 
