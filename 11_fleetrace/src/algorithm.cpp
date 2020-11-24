82062c424bdcaa75447d615b711b74d1d48d2f95
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-11-24 08:06:20 +0100

    Fleetrace finished

--- 11_fleetrace/src/algorithm.cpp
+++ 11_fleetrace/src/algorithm.cpp
@@ -6,49 +6,75 @@
 #include <algorithm>
 #include <vector>
 #include <fstream>
-// example: how to solve a simple explicit LP
-#include <CGAL/QP_models.h>
-#include <CGAL/QP_functions.h>
-#include <CGAL/Gmpz.h>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
 
-// choose input type (input coefficients must fit)
-typedef long IT;
-// choose exact type for solver (CGAL::Gmpz or CGAL::Gmpq)
-typedef CGAL::Gmpz ET;
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, traits::edge_descriptor,
+                boost::property <boost::edge_weight_t, long> > > > > graph; // new! weightmap corresponds to costs
 
-// program and solution types
-typedef CGAL::Quadratic_program<IT> Program;
-typedef CGAL::Quadratic_program_solution<ET> Solution;
+typedef boost::graph_traits<graph>::edge_descriptor             edge_desc;
+typedef boost::graph_traits<graph>::out_edge_iterator           out_edge_it; // Iterator
+
+// Custom edge adder class
+class edge_adder {
+ graph &G;
+
+ public:
+  explicit edge_adder(graph &G) : G(G) {}
+  void add_edge(int from, int to, long capacity, long cost) {
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    auto w_map = boost::get(boost::edge_weight, G); // new!
+    const edge_desc e = boost::add_edge(from, to, G).first;
+    const edge_desc rev_e = boost::add_edge(to, from, G).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = 0; // reverse edge has no capacity!
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
+    w_map[e] = cost;   // new assign cost
+    w_map[rev_e] = -cost;   // new negative cost
+  }
+};
 
 void testcase() {
 	long b, s, p; std::cin >> b >> s >> p;
 
-	// create an LP with Ax <= b, lower bound 0 and no upper bounds
-  	Program lp (CGAL::SMALLER, true, 0, false, 0); 
+	graph G(b+s+3);
+	long source = b+s;
+	long sink = b+s+1;
+	long intermediate = b+s+2;
+	edge_adder adder(G);  
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    auto rc_map = boost::get(boost::edge_residual_capacity, G);
+	
 
-	long dimNr = 0;
 	for (int i = 0; i < p; i++){
 		long bi, si, ci; std::cin >> bi >> si >> ci;
-		lp.set_a(dimNr, bi, 1);
-		lp.set_a(dimNr, b+si, 1);
-		lp.set_c(dimNr, -ci);
-		//lp.set_u(dimNr, true, 1);
-		dimNr++;
+		adder.add_edge(bi, b+si, 1, -ci+50);
+		
 	}
-	for (int i = 0; i < p; i++){
-		lp.set_b(i, 1);
+	for (int i = 0; i < b; i++){
+		adder.add_edge(i, intermediate, 1, 50);
+		adder.add_edge(source, i, 1, 0);
 	}
-	CGAL::Quadratic_program_options options;
-	options.set_pricing_strategy((CGAL::QP_BLAND));
-	// solve the program, using ET as the exact type
-  	Solution sol = CGAL::solve_linear_program(lp, ET()), options;
-	
-	if (sol.is_infeasible()){
-		std::cout << "This is kind of strange" << std::endl;
-	} else if (sol.is_optimal()){
-		long val = std::floor(CGAL::to_double(-sol.objective_value()));
-		std::cout << val << std::endl;
+	adder.add_edge(intermediate, sink, b, 0);
+	for(int i = 0; i < s; i++){
+		adder.add_edge(b+i, sink, 1, 0);
 	}
+
+	boost::successive_shortest_path_nonnegative_weights(G, source, sink);
+	long cost2 = boost::find_flow_cost(G);
+	std::cout  << -(cost2-b*50) << std::endl;
+
 	return;
 }
 
