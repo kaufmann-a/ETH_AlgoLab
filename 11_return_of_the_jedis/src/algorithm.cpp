e9d37b5b1056c886f605510dfe7ba61d0f47fec9
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-11-29 09:45:24 +0100

    Finished return of the jedi

--- 11_return_of_the_jedis/src/algorithm.cpp
+++ 11_return_of_the_jedis/src/algorithm.cpp
@@ -11,30 +11,30 @@
 #include <boost/graph/kruskal_min_spanning_tree.hpp>
 
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
-  boost::no_property, boost::property<boost::edge_weight_t, long> >      weighted_graph;
+  boost::no_property, boost::property<boost::edge_weight_t, int> >      weighted_graph;
 typedef boost::property_map<weighted_graph, boost::edge_weight_t>::type weight_map;
 typedef boost::graph_traits<weighted_graph>::edge_descriptor            edge_desc;
 typedef boost::graph_traits<weighted_graph>::vertex_descriptor          vertex_desc;
 
-bool sortEdge(std::tuple<long, long, long> &edge1, std::tuple<long, long, long> &edge2){
+bool sortEdge(std::tuple<int, int, int> &edge1, std::tuple<int, int, int> &edge2){
 	return std::get<2>(edge1) < std::get<2>(edge2);
 }
 
-long calculate_mst(std::vector<std::tuple<long, long, long>> &edges, std::vector<long> &mst_edge_indexes, long n, long nrEdgeToIgnore){
-	long cost = 0;
+int calculate_mst(std::vector<std::tuple<int, int, int>> &edges, std::vector<int> &mst_edge_indexes, int n, int nrEdgeToIgnore){
+	int cost = 0;
 	//int index_mst_edges = 0;
-	std::vector<long> trial;
 	boost::disjoint_sets_with_storage<> union_find(n);
-	for (long i = 0; i < edges.size(); i++){
+	for (int i = 0; i < edges.size(); i++){
 		if (nrEdgeToIgnore == i){
 			continue;
 		}
-		long u = union_find.find_set(std::get<0>(edges[i]));
-		long v = union_find.find_set(std::get<1>(edges[i]));
+		int u = union_find.find_set(std::get<0>(edges[i]));
+		int v = union_find.find_set(std::get<1>(edges[i]));
 		if (u != v){
 			union_find.link(u, v);
 			cost += std::get<2>(edges[i]);
-			trial.push_back((long) 12);
+			mst_edge_indexes.push_back(i);
+			if(--n == 1) break;
 			//index_mst_edges++;
 		}
 	}
@@ -43,25 +43,27 @@ long calculate_mst(std::vector<std::tuple<long, long, long>> &edges, std::vector
 
 
 void testcase() {
-	long n, index; std::cin >> n >> index;
+	int n, index; std::cin >> n >> index;
 
-	std::vector<std::tuple<long, long, long>> edges;
-	for (long j = 1; j < n; j++){
-		for (long k = 1; k <= n-j; k++){
-			long weight; std::cin >> weight;
-			edges.push_back(std::make_tuple(j, j+k, weight));
+	std::vector<std::tuple<int, int, int>> edges;
+	for(int i = 0; i < n-1; i++) {
+		for(int j = i+1; j < n; j++) {
+			int cost;
+			std::cin >> cost;
+			edges.push_back(std::make_tuple(i, j, cost));
 		}
 	}
 	
 	std::sort(edges.begin(), edges.end(), sortEdge);
-	std::vector<long> mst;
-	long cost = calculate_mst(edges, mst, n, edges.size()+1);
+	std::vector<int> mst;
+	calculate_mst(edges, mst, n, edges.size()+1);
 
-	for (long i = 0; i < mst.size(); i++){
-		long curEdgeToIgnore = mst[i];
-		std::vector<long> mst_cur;
-		long newCost = calculate_mst(edges, mst_cur, n, curEdgeToIgnore);
-		cost = std::min( cost, newCost);
+	int cost = std::numeric_limits<int>::max();
+	for (int i = 0; i < mst.size(); i++){
+		int curEdgeToIgnore = mst[i];
+		std::vector<int> mst_cur;
+		int curCost = calculate_mst(edges, mst_cur, n, curEdgeToIgnore);
+		cost = std::min(curCost, cost);
 	}
 	std::cout << cost << std::endl;
 
@@ -70,7 +72,7 @@ void testcase() {
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
-	std::fstream in("./testsets/sample.in");
+	std::fstream in("./testsets/test1.in");
 	std::cin.rdbuf(in.rdbuf());
 	
 	int t;
