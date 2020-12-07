684f9d581077984c710517d06c96e4b4e6cafc34
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-12-08 00:27:22 +0100

    Finished moving books

--- 12_moving_books/src/algorithm.cpp
+++ 12_moving_books/src/algorithm.cpp
@@ -19,37 +19,30 @@ void testcase() {
 	}
 	for (int i = 0; i < m; i++){
 		int box; std::cin >> box;
-		boxes.insert(-box);
+		boxes.insert(box);
 	}
 	std::sort(friends.begin(), friends.end());
-	std::multiset<int>::iterator beg = boxes.begin();
-	
-	if (friends[n-1] < -(*beg)){
+	std::multiset<int>::iterator end = boxes.end(); end--;
+	if (friends[n-1] < *end){
 		std::cout << "impossible" << std::endl;
 	} else {
 		int time = 0;
 		while(boxes.size() > 0){
 			for(int friend_nr = n-1; friend_nr >= 0; friend_nr--){
-				std::multiset<int>::iterator it = boxes.end();
-				it--;
-				if (*it >= -friends[friend_nr]){
-					std::multiset<int>::iterator element = boxes.find(*boxes.lower_bound(-friends[friend_nr]));
-					boxes.erase(boxes.find(*boxes.lower_bound(-friends[friend_nr])));
-					
-					// std::multiset<int>::iterator endd = boxes.end();
-					// endd--;
-					// if (*endd > friends[friend_nr]){
-					// 	//std::cout << *boxes.upper_bound(friends[friend_nr]) << std::endl;;
-					// 	std::multiset<int>::iterator element = boxes.find(*boxes.upper_bound(friends[friend_nr]));
-					// 	element--;
-					// 	boxes.erase(element);
-					// } else {
-					// 	std::multiset<int>::iterator element = boxes.end();
-					// 	element--;
-					// 	boxes.erase(element);
-					// }
-
-					//boxes.erase(boxes.find(*boxes.upper_bound(friends[friend_nr])));
+				std::multiset<int>::iterator it = boxes.begin();
+			
+				if (*it <= friends[friend_nr]){
+					std::multiset<int>::iterator endd = boxes.end();
+					endd--;
+					if (*endd > friends[friend_nr]){
+						std::multiset<int>::iterator element = boxes.find(*boxes.upper_bound(friends[friend_nr]));
+						element--;
+						boxes.erase(element);
+					} else {
+						std::multiset<int>::iterator element = boxes.end();
+						element--;
+						boxes.erase(element);
+					}
 					if (boxes.empty()){
 						break;
 					}
@@ -62,13 +55,11 @@ void testcase() {
 		}
 		std::cout << time-1 << std::endl;
 	}
-
-	
 }
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
-	std::fstream in("./testsets/sample.in");
+	std::fstream in("./testsets/test1.in");
 	std::cin.rdbuf(in.rdbuf());
 
 	int t;
