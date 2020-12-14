51caa615cbf2e720aa324616a65ba332152d7e56
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-12-14 17:13:18 +0100

    Thought i found the solution, but so wrong

---
+++ 14_secret_service/src/algorithm.cpp
@@ -0,0 +1,98 @@
+#include <limits>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
+
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, traits::edge_descriptor,
+                boost::property <boost::edge_weight_t, long> > > > > graph; // new! weightmap corresponds to costs
+
+typedef boost::graph_traits<graph>::edge_descriptor             edge_desc;
+typedef boost::graph_traits<graph>::out_edge_iterator           out_edge_it; // Iterator
+
+// Custom edge adder class
+class edge_adder {
+ graph &G;
+
+ public:
+  explicit edge_adder(graph &G) : G(G) {}
+  void add_edge(int from, int to, long capacity, long cost) {
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    auto w_map = boost::get(boost::edge_weight, G); // new!
+    const edge_desc e = boost::add_edge(from, to, G).first;
+    const edge_desc rev_e = boost::add_edge(to, from, G).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = 0; // reverse edge has no capacity!
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
+    w_map[e] = cost;   // new assign cost
+    w_map[rev_e] = -cost;   // new negative cost
+  }
+};
+
+
+void testcase() {
+	int n, m, a, s, c, d; std::cin >> n >> m >> a >> s >> c >> d;
+
+	// Create graph, edge adder class and propery maps
+    graph G(n+2);
+    edge_adder adder(G);  
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    auto rc_map = boost::get(boost::edge_residual_capacity, G);
+
+	int source = n;
+	int sink = n+1;
+	for (int i = 0; i < m; i++){
+		char w; int x, y, z; std::cin >> w >> x >> y >> z;
+		adder.add_edge(x, y, a, z);
+		if (w == 'L'){
+			adder.add_edge(y, x, a, z);
+		}
+	}
+
+	for (int i = 0; i < a; i++){
+		int alpha = 0; std::cin >> alpha;
+		adder.add_edge(source, alpha, 1, 0);
+	}
+
+	for (int i = 0; i < s; i++){
+		int sigma = 0; std::cin >> sigma;
+		adder.add_edge(sigma, sink, c, d);
+	}
+
+	boost::successive_shortest_path_nonnegative_weights(G, source, sink);
+    int cost2 = boost::find_flow_cost(G);
+
+	//Do dfs from every startingpos to find longest way 
+
+	std::cout << cost2 << std::endl;
+
+	return;
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::fstream in("./testsets/sample.in");
+	std::cin.rdbuf(in.rdbuf());
+
+	int t;
+	std::cin >> t;
+	for (int i = 0; i < t; ++i)
+		testcase();
+	return 0;
+}
