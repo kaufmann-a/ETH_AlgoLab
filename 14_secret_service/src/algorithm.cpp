2df29f1df10b9ed0e2ae5a0c8b65a90261d4839c
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-12-15 13:34:46 +0100

    Next try, 80%

--- 14_secret_service/src/algorithm.cpp
+++ 14_secret_service/src/algorithm.cpp
@@ -30,47 +30,24 @@ typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost:
 typedef boost::graph_traits<graph>::edge_descriptor             edge_desc_flow;
 typedef boost::graph_traits<graph>::edge_iterator           edge_it_flow; // Iterator
 
-// Custom edge adder class
+// Custom edge adder class, highly recommended
 class edge_adder {
- graph &G;
+  graph &G;
 
  public:
   explicit edge_adder(graph &G) : G(G) {}
-  void add_edge(int from, int to, long capacity, long cost) {
+
+  void add_edge(int from, int to, long capacity) {
     auto c_map = boost::get(boost::edge_capacity, G);
     auto r_map = boost::get(boost::edge_reverse, G);
-    auto w_map = boost::get(boost::edge_weight, G); // new!
-    const edge_desc e = boost::add_edge(from, to, G).first;
-    const edge_desc rev_e = boost::add_edge(to, from, G).first;
+    const auto e = boost::add_edge(from, to, G).first;
+    const auto rev_e = boost::add_edge(to, from, G).first;
     c_map[e] = capacity;
     c_map[rev_e] = 0; // reverse edge has no capacity!
     r_map[e] = rev_e;
     r_map[rev_e] = e;
-    w_map[e] = cost;   // new assign cost
-    w_map[rev_e] = -cost;   // new negative cost
   }
 };
-
-int dijkstra_path(const weighted_graph &G, int s, int t, std::vector<vertex_desc> &path) {
-  int n = boost::num_vertices(G);
-  std::vector<int>         dist_map(n);
-  std::vector<vertex_desc> pred_map(n);
-
-  boost::dijkstra_shortest_paths(G, s,
-    boost::distance_map(boost::make_iterator_property_map(
-      dist_map.begin(), boost::get(boost::vertex_index, G)))
-    .predecessor_map(boost::make_iterator_property_map(
-      pred_map.begin(), boost::get(boost::vertex_index, G))));
-
-  int cur = t;
-  path.clear(); path.push_back(cur);
-  while (s != cur) {
-    cur = pred_map[cur];
-    path.push_back(cur);
-  }
-  std::reverse(path.begin(), path.end());
-  return dist_map[t];
-}
 
 void testcase() {
 	int n, m, a, s, c, d; std::cin >> n >> m >> a >> s >> c >> d;
@@ -97,14 +74,7 @@ void testcase() {
 		std::cin >> shelters[i];
 	}
 
-	// Create graph, edge adder class and propery maps
-    graph G_flow(2*n+2);
-    edge_adder adder(G_flow);  
-    auto c_map = boost::get(boost::edge_capacity, G_flow);
-    auto r_map = boost::get(boost::edge_reverse, G_flow);
-    auto rc_map = boost::get(boost::edge_residual_capacity, G_flow);
-	int source = 2*n;
-	int sink = 2*n+1;
+	std::vector<std::pair<int, std::vector<int>>> distMaps;
 	for (int i = 0; i < a; i++){
 		//Run dijkstra for every agent
 		int startNode = agents[i];
@@ -115,47 +85,52 @@ void testcase() {
 			  boost::get(boost::vertex_index, G))).predecessor_map(
 				  boost::make_iterator_property_map(pred_map.begin(), 
 				  boost::get(boost::vertex_index, G))));
-		
-		adder.add_edge(source, startNode, 1, 0);
-
-		//Iterate over all rachable targets and add shortest distance
-		for (int j = 0; j < s; j++){
-			int curShelter = shelters[j];
-			if (dist_map[curShelter] != std::numeric_limits<int>::max()){
-				adder.add_edge(startNode, n+curShelter, 1, dist_map[curShelter]);
-			}
-		}
+		distMaps.push_back(std::make_pair(startNode, dist_map));
 	}
 
-	for (int i = 0; i < s; i++){
-		int curShelter = shelters[i];
-		adder.add_edge(n+curShelter, sink, c, 0);
-	}
+	//Do binarysearch on the time needed
+	int l = 0;
+	int r = std::numeric_limits<int>::max()-1;
+	while(l < r){
+		int mid = l + (r-l)/2;
 
-	boost::successive_shortest_path_nonnegative_weights(G_flow, source, sink);
-
-	edge_it_flow e_beg, e_end;
-	int longest_edge = 0;
-	for (boost::tie(e_beg, e_end) = boost::edges(G_flow); e_beg != e_end; ++e_beg){
-		int s_flow = c_map[*e_beg] - rc_map[*e_beg];
-		if (s_flow > 0){
-			auto w_map = boost::get(boost::edge_weight, G_flow);
-			int cost = w_map[*e_beg];
-			if (cost > longest_edge){
-				longest_edge = cost;
+		//Build flowgraph, just add edges with time-d <= mid
+		int source = 2*n;
+		int sink = 2*n+1;
+		graph G_flow(2*n+2);
+    	edge_adder adder(G_flow);
+		for (int i = 0; i < a; i++){
+			int curAgent = distMaps[i].first;
+			adder.add_edge(source, curAgent, 1);
+			std::vector<int> curDistMap = distMaps[i].second;
+			for (int j = 0; j < shelters.size(); j++){
+				int curShelter = shelters[j];
+				if(curDistMap[curShelter] <= (mid-d)){
+					adder.add_edge(curAgent, n+curShelter, 1);
+				}
 			}
 		}
 
+		for (int i = 0; i < s; i++){
+			int curShelter = shelters[i];
+			adder.add_edge(curShelter+n, sink, c);
+		}
+
+		long flow = boost::push_relabel_max_flow(G_flow, source, sink);
+		if (flow < a){ //Not enought time was provided
+			l = mid+1;
+		} else if (flow == a){
+			r = mid;
+		}
+
 	}
-
-	std::cout << longest_edge + d << std::endl;
-
+	std::cout << r << std::endl;	
 	return;
 }
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
-	std::fstream in("./testsets/sample.in");
+	std::fstream in("./testsets/user_test.in");
 	std::cin.rdbuf(in.rdbuf());
 
 	int t;
