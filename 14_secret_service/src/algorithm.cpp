e48df5eb363de6e3b9a21c9331eb9ac27e27a68d
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-12-15 10:54:37 +0100

    next try, works neither

--- 14_secret_service/src/algorithm.cpp
+++ 14_secret_service/src/algorithm.cpp
@@ -7,11 +7,18 @@
 #include <vector>
 #include <fstream>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/cycle_canceling.hpp>
 #include <boost/graph/push_relabel_max_flow.hpp>
 #include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
 #include <boost/graph/find_flow_cost.hpp>
 
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+  boost::no_property, boost::property<boost::edge_weight_t, int> >      weighted_graph;
+typedef boost::property_map<weighted_graph, boost::edge_weight_t>::type weight_map;
+typedef boost::graph_traits<weighted_graph>::edge_descriptor            edge_desc;
+typedef boost::graph_traits<weighted_graph>::vertex_descriptor          vertex_desc;
 // Graph Type with nested interior edge properties for Cost Flow Algorithms
 typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
@@ -20,8 +27,8 @@ typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost:
             boost::property<boost::edge_reverse_t, traits::edge_descriptor,
                 boost::property <boost::edge_weight_t, long> > > > > graph; // new! weightmap corresponds to costs
 
-typedef boost::graph_traits<graph>::edge_descriptor             edge_desc;
-typedef boost::graph_traits<graph>::out_edge_iterator           out_edge_it; // Iterator
+typedef boost::graph_traits<graph>::edge_descriptor             edge_desc_flow;
+typedef boost::graph_traits<graph>::edge_iterator           edge_it_flow; // Iterator
 
 // Custom edge adder class
 class edge_adder {
@@ -44,43 +51,104 @@ class edge_adder {
   }
 };
 
+int dijkstra_path(const weighted_graph &G, int s, int t, std::vector<vertex_desc> &path) {
+  int n = boost::num_vertices(G);
+  std::vector<int>         dist_map(n);
+  std::vector<vertex_desc> pred_map(n);
+
+  boost::dijkstra_shortest_paths(G, s,
+    boost::distance_map(boost::make_iterator_property_map(
+      dist_map.begin(), boost::get(boost::vertex_index, G)))
+    .predecessor_map(boost::make_iterator_property_map(
+      pred_map.begin(), boost::get(boost::vertex_index, G))));
+
+  int cur = t;
+  path.clear(); path.push_back(cur);
+  while (s != cur) {
+    cur = pred_map[cur];
+    path.push_back(cur);
+  }
+  std::reverse(path.begin(), path.end());
+  return dist_map[t];
+}
 
 void testcase() {
 	int n, m, a, s, c, d; std::cin >> n >> m >> a >> s >> c >> d;
 
-	// Create graph, edge adder class and propery maps
-    graph G(n+2);
-    edge_adder adder(G);  
-    auto c_map = boost::get(boost::edge_capacity, G);
-    auto r_map = boost::get(boost::edge_reverse, G);
-    auto rc_map = boost::get(boost::edge_residual_capacity, G);
+	weighted_graph G(n);
+  	weight_map weights = boost::get(boost::edge_weight, G);
 
-	int source = n;
-	int sink = n+1;
 	for (int i = 0; i < m; i++){
 		char w; int x, y, z; std::cin >> w >> x >> y >> z;
-		adder.add_edge(x, y, a, z);
+		edge_desc e1;
+		e1 = boost::add_edge(x, y, G).first; weights[e1]=z;
 		if (w == 'L'){
-			adder.add_edge(y, x, a, z);
+			edge_desc e2;
+			e2 = boost::add_edge(y, x, G).first; weights[e2] = z;
 		}
 	}
 
+	std::vector<int> agents(a, 0);
 	for (int i = 0; i < a; i++){
-		int alpha = 0; std::cin >> alpha;
-		adder.add_edge(source, alpha, 1, 0);
+		std::cin >> agents[i];
+	}
+	std::vector<int> shelters(s, 0);
+	for (int i = 0; i < s; i++){
+		std::cin >> shelters[i];
+	}
+
+	// Create graph, edge adder class and propery maps
+    graph G_flow(2*n+2);
+    edge_adder adder(G_flow);  
+    auto c_map = boost::get(boost::edge_capacity, G_flow);
+    auto r_map = boost::get(boost::edge_reverse, G_flow);
+    auto rc_map = boost::get(boost::edge_residual_capacity, G_flow);
+	int source = 2*n;
+	int sink = 2*n+1;
+	for (int i = 0; i < a; i++){
+		//Run dijkstra for every agent
+		int startNode = agents[i];
+  		std::vector<int> dist_map(n, -1);
+  		std::vector<vertex_desc> pred_map(n);
+  		boost::dijkstra_shortest_paths(G, startNode, boost::distance_map(
+			  boost::make_iterator_property_map(dist_map.begin(), 
+			  boost::get(boost::vertex_index, G))).predecessor_map(
+				  boost::make_iterator_property_map(pred_map.begin(), 
+				  boost::get(boost::vertex_index, G))));
+		
+		adder.add_edge(source, startNode, 1, 0);
+
+		//Iterate over all rachable targets and add shortest distance
+		for (int j = 0; j < s; j++){
+			int curShelter = shelters[j];
+			if (dist_map[curShelter] != std::numeric_limits<int>::max()){
+				adder.add_edge(startNode, n+curShelter, 1, dist_map[curShelter]);
+			}
+		}
 	}
 
 	for (int i = 0; i < s; i++){
-		int sigma = 0; std::cin >> sigma;
-		adder.add_edge(sigma, sink, c, d);
+		int curShelter = shelters[i];
+		adder.add_edge(n+curShelter, sink, c, 0);
 	}
 
-	boost::successive_shortest_path_nonnegative_weights(G, source, sink);
-    int cost2 = boost::find_flow_cost(G);
+	boost::successive_shortest_path_nonnegative_weights(G_flow, source, sink);
 
-	//Do dfs from every startingpos to find longest way 
+	edge_it_flow e_beg, e_end;
+	int longest_edge = 0;
+	for (boost::tie(e_beg, e_end) = boost::edges(G_flow); e_beg != e_end; ++e_beg){
+		int s_flow = c_map[*e_beg] - rc_map[*e_beg];
+		if (s_flow > 0){
+			auto w_map = boost::get(boost::edge_weight, G_flow);
+			int cost = w_map[*e_beg];
+			if (cost > longest_edge){
+				longest_edge = cost;
+			}
+		}
 
-	std::cout << cost2 << std::endl;
+	}
+
+	std::cout << longest_edge + d << std::endl;
 
 	return;
 }
