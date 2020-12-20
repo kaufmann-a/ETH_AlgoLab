60a0e08adfa7b1da189632aa9104ee1cae2c0499
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2020-12-20 13:28:46 +0100

    Hand, testset1

--- 11_hand/src/algorithm.cpp
+++ 11_hand/src/algorithm.cpp
@@ -11,64 +11,99 @@
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
 #include <CGAL/Triangulation_face_base_2.h>
 #include <boost/pending/disjoint_sets.hpp>
-#include <vector>
-#include <tuple>
-#include <algorithm>
-#include <iostream>
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/max_cardinality_matching.hpp>
+#include <boost/graph/connected_components.hpp>
 
-// Epic kernel is enough, no constructions needed, provided the squared distance
-// fits into a double (!)
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-// we want to store an index with each vertex
 typedef std::size_t                                            Index;
 typedef CGAL::Triangulation_vertex_base_with_info_2<Index,K>   Vb;
 typedef CGAL::Triangulation_face_base_2<K>                     Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb>            Tds;
 typedef CGAL::Delaunay_triangulation_2<K,Tds>                  Delaunay;
+typedef std::tuple<Index,Index,K::FT> 						   Edge;
+typedef std::vector<Edge> 									   EdgeV;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>		graph;
+typedef boost::graph_traits<graph>::vertex_descriptor		   vertex_desc;
+typedef boost::graph_traits<graph>::edge_iterator			   edge_it;
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> graph;
-typedef boost::graph_traits<graph>::vertex_descriptor                       vertex_desc;
-
-bool mySort(std::pair<K::Point_2, long> &p1, std::pair<K::Point_2, long> &p2){
-	return p1.second < p2.second;
+std::ostream& operator<<(std::ostream& o, const Edge& e) {
+	return o << std::get<0>(e) << " " << std::get<1>(e) << " " << std::get<2>(e);
 }
 
-void testcase() {
-	int n, k, f; double s; std::cin >> n >> k >> f >> s;
-
-	typedef std::pair<K::Point_2,Index> IPoint;
-	std::vector<IPoint> tents;
-	tents.reserve(n);
-
-	for (int i = 0; i < n; i++){
-		int x, y; std::cin >> x >> y;
-		tents.push_back(std::make_pair(K::Point_2(x, y), i));
-	}
-	Delaunay t;
-	t.insert(tents.begin(), tents.end());
-
-	double smallestDist = std::numeric_limits<double>::max(); 
+int nrFamilies(Delaunay &del, K::FT &givenSquaredDist, int n, int k){
 	graph G(n);
-	for (auto e = t.finite_edges_begin(); e != t.finite_edges_end(); ++e) {
+	//First compute a graph containing all edges of delaunay with squared length >= s
+	for (auto e = del.finite_edges_begin(); e != del.finite_edges_end(); ++e) {
     	Index i1 = e->first->vertex((e->second+1)%3)->info();
-   		Index i2 = e->first->vertex((e->second+2)%3)->info();
-		double dist = CGAL::to_double(t.segment(e).squared_length()); 
-		if (dist < smallestDist) {
-			smallestDist = dist;
-		}
-		if (dist < s){
+    	Index i2 = e->first->vertex((e->second+2)%3)->info();
+		if (del.segment(e).squared_length() <= givenSquaredDist){
 			boost::add_edge(i1, i2, G);
 		}
   	}
 	
-	std::vector<vertex_desc> mate_map(n);  // exterior property map
-  	const vertex_desc NULL_VERTEX = boost::graph_traits<graph>::null_vertex();
-  	boost::edmonds_maximum_cardinality_matching(G, boost::make_iterator_property_map(mate_map.begin(), boost::get(boost::vertex_index, G)));
-  	int matching_size = boost::matching_size(G, boost::make_iterator_property_map(mate_map.begin(), boost::get(boost::vertex_index, G)));
+	std::vector<int> component_map(n);	// We MUST use such a vector as an Exterior Property Map: Vertex -> Component
+	int ncc = boost::connected_components(G, boost::make_iterator_property_map(component_map.begin(), boost::get(boost::vertex_index, G))); 
+	
+	std::vector<std::vector<vertex_desc>> component_vertices(ncc);
+	// Iterate over all vertices
+	for (int i = 0; i < n; ++i)
+		component_vertices[component_map[i]].push_back(i);
 
-	std::cout << smallestDist << " " << n-matching_size << std::endl;
+	// Iterate over all components
+	int countNrFamilies = 0; //This is the answer of the second question
+	for (int c_i = 0; c_i < ncc; ++c_i){
+		if (component_vertices[c_i].size() >= k){
+			countNrFamilies++;
+		}
+	}
+	return countNrFamilies;
+}
+
+void testcase() {
+	long n, k, f, s; std::cin >> n >> k >> f >> s;
+
+	typedef std::pair<K::Point_2,Index> IPoint;
+  	std::vector<IPoint> points;
+  	points.reserve(n);
+  	for (Index i = 0; i < n; ++i) {
+		long x, y;
+		std::cin >> x >> y;
+		points.emplace_back(K::Point_2(x, y), i);
+	}
+	Delaunay t;
+	t.insert(points.begin(), points.end());
+
+
+	EdgeV edges;
+  	edges.reserve(3*n); // there can be no more in a planar graph
+  	for (auto e = t.finite_edges_begin(); e != t.finite_edges_end(); ++e) {
+		Index i1 = e->first->vertex((e->second+1)%3)->info();
+		Index i2 = e->first->vertex((e->second+2)%3)->info();
+		// ensure smaller index comes first
+    	if (i1 > i2) std::swap(i1, i2);
+    	edges.emplace_back(i1, i2, t.segment(e).squared_length());
+  	}
+  	std::sort(edges.begin(), edges.end(), [](const Edge& e1, const Edge& e2) -> bool {
+		  return std::get<2>(e1) <std::get<2>(e2);
+	  });
+
+	int l = 0; int r = edges.size()-1;
+	while (l < r){
+		int mid = l + (r- l) / 2;
+		K::FT curSquaredDist = std::get<2>(edges[mid]);
+		int curNrFamilies = nrFamilies(t, curSquaredDist, n, k);
+		if (curNrFamilies < f){
+			r = mid-1;
+		} else if (curNrFamilies >= f){
+			l = mid;
+		}
+	}
+	long q1 = std::get<2>(edges[l]);
+
+	K::FT squaredDist = s;
+	int q2 = nrFamilies(t, squaredDist, n, k);
+	
+	std::cout << q1 << " " << q2 << std::endl;
 	return;
 }
 
@@ -76,7 +111,7 @@ int main() {
 	std::ios_base::sync_with_stdio(false);
 	std::fstream in("./testsets/test1.in");
 	std::cin.rdbuf(in.rdbuf());
-
+	
 	int t;
 	std::cin >> t;
 	for (int i = 0; i < t; ++i)
