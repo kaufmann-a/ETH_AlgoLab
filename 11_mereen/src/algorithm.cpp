9e21b7544fe3091e4a2273a98ad901d0194322ce
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2021-01-16 09:07:18 +0100

    Fourth testset meereen running

--- 11_mereen/src/algorithm.cpp
+++ 11_mereen/src/algorithm.cpp
@@ -9,14 +9,6 @@
 #include <set>
 #include <map>
 
-//dp_entry is used to save a dp_entry after n rounds, which fighters are on the left, which fighters are on the right
-struct dp_entry{
-	int left_m; //encoding of last m fighters. example: m=3, if left_m = 34 this means that last figher was of type 4 and second to last of type 3
-	int right_m;
-	long sum_excitement;
-};
-
-
 int calculate_nr_diff_fighters(int nr){
 	std::vector<int> fighter_dist(5);
 	while (nr > 0){
@@ -48,18 +40,18 @@ void testcase() {
 	//dim1: represents ratio total left to toal right moves. for dim1=12 this means equal amount of left and right, for dim1<12, more left, for dim1>12 more right
 	//dim2 set: here we store different possible dp_entry, per dp_entry always the one with highest excitement is kept
 	//tuple: <0>=left_queue, <1>=right_queue <2>=total excitement
-	std::vector<std::vector<std::map<std::pair<int, int>, long>>> dp(n+1, std::vector<std::map<std::pair<int, int>, long>>(25));
+	int dim2 = 25;
+	std::vector<std::vector<std::map<std::pair<int, int>, long>>> dp(n+1, std::vector<std::map<std::pair<int, int>, long>>(dim2));
 
 	//init dp
-	
+	int equal_left_right = dim2/2;
 	std::pair<int, int> entry = std::make_pair(0, 0);
-	dp[0][12].insert(std::make_pair(entry, 0));
-	int equal_left_right = 12;
-
+	dp[0][equal_left_right].insert(std::make_pair(entry, 0));
+	
 	//now we iterate over the queue
 	for (int i = 0; i< n; i++){
 		int next_fighter = fighters[i];
-		for (int j = 0; j < 25; j++){
+		for (int j = 0; j < dim2; j++){
 			
 			//check if dp[i][j] contains a dp_entry
 			if (dp[i][j].empty()){
@@ -71,27 +63,29 @@ void testcase() {
 				std::pair<std::pair<int, int>, long> cur_entry = *it;
 				
 				//possibility 1: we move to the left
-				int left_queue = (cur_entry.first.first*10+next_fighter) % (int) (std::pow(10, m));
+				int left_queue = cur_entry.first.first*10+next_fighter;
 				long excitement_l = 1000 * calculate_nr_diff_fighters(left_queue);
 				long penalty_l = std::pow(2, std::abs(equal_left_right - j + 1));
 				long final_excitement_l = excitement_l - penalty_l;
 				//check if in dp[i+1][j-1] queue with set already there
 				if (final_excitement_l > 0){
 					long new_total_exitement_l = cur_entry.second + final_excitement_l;
-					if (new_total_exitement_l > dp[i+1][j-1][std::make_pair(left_queue, cur_entry.first.second)]){
-						dp[i+1][j-1][std::make_pair(left_queue, cur_entry.first.second)] = new_total_exitement_l;
+					int left_queue_cutted = left_queue % (int) (std::pow(10, (m-1)));
+					if (new_total_exitement_l > dp[i+1][j-1][std::make_pair(left_queue_cutted, cur_entry.first.second)]){
+						dp[i+1][j-1][std::make_pair(left_queue_cutted, cur_entry.first.second)] = new_total_exitement_l;
 					}
 				}
 
 				//possibility 2: we move to the right
-				int right_queue = (cur_entry.first.second*10+next_fighter) % (int) (std::pow(10, m));
+				int right_queue = cur_entry.first.second*10+next_fighter;
 				long excitement_r = 1000 * calculate_nr_diff_fighters(right_queue);
 				long penalty_r = std::pow(2, std::abs(equal_left_right - j - 1));
 				long final_excitement_r = excitement_r - penalty_r;
 				if (final_excitement_r > 0){
 					long new_total_excitement_r = cur_entry.second + final_excitement_r;
-					if (new_total_excitement_r > dp[i+1][j+1][std::make_pair(cur_entry.first.first, right_queue)]){
-						dp[i+1][j+1][std::make_pair(cur_entry.first.first, right_queue)] = new_total_excitement_r;
+					int right_queue_cutted = right_queue % (int) (std::pow(10, (m-1)));
+					if (new_total_excitement_r > dp[i+1][j+1][std::make_pair(cur_entry.first.first , right_queue_cutted)]){
+						dp[i+1][j+1][std::make_pair(cur_entry.first.first, right_queue_cutted)] = new_total_excitement_r;
 					}
 				}
 			}
@@ -99,7 +93,7 @@ void testcase() {
 	}
 
 	long maxExcitement = 0;
-	for (int i = 0; i < 25; i++){
+	for (int i = 0; i < dim2; i++){
 		for (std::map<std::pair<int, int>, long>::iterator it = dp[n][i].begin(); it != dp[n][i].end(); it++){
 			if (it->second > maxExcitement){
 				maxExcitement = it->second;
