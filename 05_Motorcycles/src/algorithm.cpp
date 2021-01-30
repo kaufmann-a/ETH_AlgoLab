eae970d971dc4e98b223369fa441c84f62266110
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2021-01-30 13:54:20 +0100

    solved motorcycles

--- 05_Motorcycles/src/algorithm.cpp
+++ 05_Motorcycles/src/algorithm.cpp
@@ -6,144 +6,113 @@
 #include <algorithm>
 #include <vector>
 #include <fstream>
-#include <map>
-#include <set>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/squared_distance_2.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel IK;
 typedef CGAL::Exact_predicates_exact_constructions_kernel EK;
 
-double floor_to_double(const EK::FT& x){
-	double a = std::floor(CGAL::to_double(x));
-	while (a > x) a -= 1;
-	while (a+1 <= x) a +=1;
-	return a;
+int UP = 1; int DOWN = -1;
+
+struct Biker{
+
+	int index = 0;
+	long y_start_pos = 0;
+	int up_down = 0; //-1 = down, 1 = up
+	EK::FT slope = 0;
+
+	Biker(int ind, long stps, int ud, EK::FT sl) : index(ind), y_start_pos(stps), up_down(ud), slope(sl){};
+};
+
+bool bikerSort (const Biker &biker1, const Biker &biker2){
+	return biker1.y_start_pos > biker2.y_start_pos;
 }
 
-void intersectCheck(std::vector<EK::Segment_2> &segments, std::map<int, EK::Ray_2> &rays, EK::Ray_2 &curRay, int bicker_nr){
-
-	bool collidedWithSeg = false;
-	EK::Segment_2 ray_seg;
-	for (ulong i = 0; i < segments.size(); i++){ //Loop over every already read biker and check if ray somewhere intersects
-		bool doIntersect = false;
-		if (collidedWithSeg){
-			doIntersect = CGAL::do_intersect(segments[i], ray_seg);
-			std::cout << segments[ï].source().x() << " " << segments[i].source().y() << std::endl;
-		} else {
-			doIntersect = CGAL::do_intersect(segments[i], curRay);
-		}
-		
-		
-		if (doIntersect){ //Intersection check
-			auto o = CGAL::intersection(segments[i], curRay);
-			if (const EK::Point_2* op = boost::get<EK::Point_2>(&*o)){
-				//Segment and ray intersect, check which distance to origin is larger
-				EK::FT dist_ray = CGAL::squared_distance(*op, curRay.source());
-				EK::FT dist_seg = CGAL::squared_distance(*op, segments[i].source());
-				if (dist_ray > dist_seg){ //new rays distance is longer, thus journey ends at this point op
-					collidedWithSeg = true;
-					ray_seg.source() = curRay.source();
-					ray_seg.target() = *op;
-				} else if (dist_ray == dist_seg){ //Case where both distances are the same, check which bicker came from the right
-					if (curRay.source().y() < segments[i].source().y()){ //new Ray wins, old seg is shortened
-						EK::Segment_2 seg_new(segments[i].source(), *op);
-						segments[i] = seg_new;
-					} else { //Old seg wins, new ray is shortened
-						collidedWithSeg = true;
-						ray_seg.source() = curRay.source();
-						ray_seg.target() = *op;
-					}
-				} else { //Case where old seg is longer, therefore ray stays
-					EK::Segment_2 seg_new(segments[i].source(), *op);
-					segments[i] = seg_new;
-				}
-			} else {
-				std::cout << "What is happening here" << std::endl;
-			}
-		}
-	}
-
-	if (collidedWithSeg){ //No potential driver to sunset, therefore add segment of driver to segments-array
-		segments.push_back(ray_seg);
-	} else { //No coolision, could be potential new driver to sunset, check if collision with other pot. sunsetdrivers
-		bool newPotDriver = true;
-		bool colided = false;
-		EK::Segment_2 ray_seg_new;
-		for (auto it = rays.cbegin(), next_it = it; it != rays.cend(); it = next_it){
-			++next_it;
-			bool doIntersect = false;
-			if (colided){
-				doIntersect = CGAL::do_intersect(it->second, ray_seg_new);
-			} else {
-				doIntersect = CGAL::do_intersect(it->second, curRay);
-			}
-				
-			if (doIntersect){
-				auto o = CGAL::intersection(it->second, curRay);
-				if (const EK::Point_2* op = boost::get<EK::Point_2>(&*o)){
-					//Rays intersect, check which distance to origin is larger
-					EK::FT dist_new_ray = CGAL::squared_distance(*op, curRay.source());
-					EK::FT dist_old_ray = CGAL::squared_distance(*op, it->second.source());
-
-					if (dist_new_ray > dist_old_ray){ //New ray looses and is added as segment to segments array
-						EK::Segment_2 seg(curRay.source(), *op);
-						ray_seg_new = seg;
-						newPotDriver = false;
-						colided = true;
-					} else { //Old pot driver to sunset is beaten, remove from rays and insert to sesgments
-						EK::Segment_2 new_seg(it->second.source(), *op);
-						segments.push_back(new_seg);
-						rays.erase(it);
-					}
-				} else {
-					std::cout << "Again strange stuff here" << std::endl;
-				}
-			}
-		}
-		if (newPotDriver){
-			rays.insert(std::make_pair(bicker_nr, curRay));
-		} else {
-			segments.push_back(ray_seg_new);
-		}
-
-	}
-
-
+bool bikerSortIndex (const Biker &biker1, const Biker &biker2){
+	return biker1.index < biker2.index;
 }
-
 
 void testcase() {
 	int n; std::cin >> n;
 
-	std::vector<EK::Segment_2> segments;
-	std::map<int, EK::Ray_2> rays;
+	std::vector<Biker> bikers;
+	for (int i = 0; i < n; i++){
+		long y0, x1, y1; std::cin >> y0 >> x1 >> y1;
+
+		int up_down = 0;
+		//up_down:
+		if (y0 <= y1){
+			up_down = UP;
+		} else {
+			up_down = DOWN;
+		}
+		EK::FT y_segment = std::abs(y1-y0);
+		EK::FT x1_exact = x1;
+		EK::FT slope = y_segment/x1_exact;
+		Biker cur_biker(i, y0, up_down, slope);
+		bikers.push_back(cur_biker);
+	}
+
+	//Sort biker according to their start position, biker with highest y0 comes first
+	std::sort(bikers.begin(), bikers.end(), bikerSort);
+
+	std::vector<Biker> sunriseBikers;
 
 	for (int i = 0; i < n; i++){
-		long x0, y0, x1, y1; std::cin >> y0; std::cin >> x1; std::cin >> y1; x0 = 0;
-		EK::Point_2 start(x0, y0), sup(x1, y1);
-		EK::Ray_2 ray(start, sup);
-		intersectCheck(segments, rays, ray, i);
+		//First check: is sunriseBikers array empty, if so just add the current biker
+
+
+		Biker curBiker = bikers[i];
+		bool curBikerDone = false;
+		while (!curBikerDone){
+			if (sunriseBikers.empty()){
+				sunriseBikers.push_back(curBiker);
+				break;
+			}
+			Biker lastSunriseBiker = sunriseBikers[sunriseBikers.size()-1];
+			if (curBiker.up_down == UP){ // CurBiker rides up
+				if (lastSunriseBiker.up_down == UP){
+					if (lastSunriseBiker.slope >= curBiker.slope){
+						sunriseBikers.push_back(curBiker);
+						curBikerDone = true;
+					} else if (lastSunriseBiker.slope < curBiker.slope){
+						curBikerDone = true;
+					}
+				} else if (lastSunriseBiker.up_down == DOWN){
+					if (lastSunriseBiker.slope  >= curBiker.slope){
+						sunriseBikers.pop_back();
+					} else if (lastSunriseBiker.slope < curBiker.slope){
+						curBikerDone = true;
+					}
+				}
+			} else if (curBiker.up_down == DOWN){ //Cur biker rides down
+				if (lastSunriseBiker.up_down == UP){
+					sunriseBikers.push_back(curBiker);
+					curBikerDone = true;
+				} else if (lastSunriseBiker.up_down == DOWN){
+					if (lastSunriseBiker.slope > curBiker.slope){
+						sunriseBikers.pop_back();
+					} else if (lastSunriseBiker.slope <= curBiker.slope){
+						sunriseBikers.push_back(curBiker);
+						curBikerDone = true;
+					}
+				}
+			}
+		}
 	}
-	 
-	for (std::map<int, EK::Ray_2>::iterator it = rays.begin(); it != rays.end(); it++){
-		std::cout << it->first << " ";
+
+	std::sort(sunriseBikers.begin(), sunriseBikers.end(), bikerSortIndex);
+
+	for (uint i = 0; i < sunriseBikers.size(); i++){
+		std::cout << sunriseBikers[i].index << " ";
 	}
 	std::cout << std::endl;
-
-
-
-
 
 	return;
 }
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
-	std::fstream in ("./testsets/test1.in");
+	std::fstream in("./testsets/sample.in");
 	std::cin.rdbuf(in.rdbuf());
-
 
 	int t;
 	std::cin >> t;
