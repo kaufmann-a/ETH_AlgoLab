0964ec1b420ce0799ec12a04b014eaea88dd190a
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2021-01-31 15:57:15 +0100

    Reimplementation idefix

--- 10_idefix/src/algorithm.cpp
+++ 10_idefix/src/algorithm.cpp
@@ -11,13 +11,9 @@
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
 #include <CGAL/Triangulation_face_base_2.h>
 #include <boost/pending/disjoint_sets.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/connected_components.hpp>
+#include <set>
 
-// Epic kernel is enough, no constructions needed, provided the squared distance
-// fits into a double (!)
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-// we want to store an index with each vertex
 typedef std::size_t                                            Index;
 typedef CGAL::Triangulation_vertex_base_with_info_2<Index,K>   Vb;
 typedef CGAL::Triangulation_face_base_2<K>                     Fb;
@@ -27,162 +23,126 @@ typedef CGAL::Delaunay_triangulation_2<K,Tds>                  Delaunay;
 typedef std::tuple<Index,Index,K::FT> Edge;
 typedef std::vector<Edge> EdgeV;
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>					graph;
-typedef boost::graph_traits<graph>::vertex_descriptor		vertex_desc;		// Vertex Descriptor: with vecS vertex list, this is really just an int in the range [0, num_vertices(G)).	
-typedef boost::graph_traits<graph>::edge_iterator		edge_it;		// to iterate over all edges
-
-bool mySort (std::pair<int, long> &bone1, std::pair<int, long> &bone2){
-	return bone1.second < bone2.second;
-}
-
-
-int nrBones (Delaunay t, long n, long s, std::vector<std::pair<int, long>> &bones_locations, EdgeV &edges){
-	// reachable bones per tree
-    std::vector<int> bones_count(n);
-    for (auto &bone : bones_locations)
-    {
-        if (bone.second * 4 <= K::FT(s))
-        {
-            bones_count[bone.first]++;
-        }
-    }
-	
-	boost::disjoint_sets_with_storage<> uf(n);
-	Index n_components = n;
+int nrBones(EdgeV &edges, std::vector<std::multiset<long>> bones_to_tree, long s){
+	//First build component array
+	int nrTrees = bones_to_tree.size();
+	int curMaxCompSize = 0;
+	std::vector<int> components(nrTrees);
+	for (int i = 0; i < nrTrees; i++){
+		for (auto it = bones_to_tree[i].begin(); it != bones_to_tree[i].end(); it++){
+			long curDist = *it;
+			if (curDist <= s){
+				components[i]++;
+				if (components[i] > curMaxCompSize){
+					curMaxCompSize = components[i];
+				}
+			} else {
+				break;
+			}
+		}
+	}
+	// setup and initialize union-find data structure
+	boost::disjoint_sets_with_storage<> uf(nrTrees);
+	Index n_components = nrTrees;
 	// ... and process edges in order of increasing length
 	for (EdgeV::const_iterator e = edges.begin(); e != edges.end(); ++e) {
+		if (std::get<2>(*e) > s){ //Stop building emst
+			break;
+		}
+		
 		// determine components of endpoints
 		Index c1 = uf.find_set(std::get<0>(*e));
 		Index c2 = uf.find_set(std::get<1>(*e));
 		if (c1 != c2) {
 			// this edge connects two different components => part of the emst
 			uf.link(c1, c2);
-			if (std::get<2>(*e) <= s){
-				// edge is not to long -> combine the bone count of both components
-                int combined_bone_count = bones_count[c1] + bones_count[c2];
-                bones_count[c1] = combined_bone_count;
-                bones_count[c2] = combined_bone_count;
+			int bonesInComp = components[c1] + components[c2];
+			Index parent = uf.find_set(c1);
+			if (parent == c1){
+				components[c1] = bonesInComp; components[c2] = 0;
+			} else {
+				components[c2] = bonesInComp; components[c1] = 0;
+			}
+			if (bonesInComp > curMaxCompSize){
+				curMaxCompSize = bonesInComp;
 			}
 			if (--n_components == 1) break;
 		}
 	}
-	size_t max_count = 0;
-    for (size_t count : bones_count)
-    {
-        max_count = std::max(max_count, count);
-    }
-	return max_count;
+	return curMaxCompSize;
 }
 
-void testcase() {
-	long n, m, s, k; std::cin >> n >> m >> s >> k;
 
-	//Read in all oak trees and build delaunay triangulation
+void testcase() {
+	int n, m; long s; int k; std::cin >> n >> m >> s >> k;
+
 	typedef std::pair<K::Point_2,Index> IPoint;
 	std::vector<IPoint> points;
 	points.reserve(n);
-	for (Index i = 0; i < n; ++i) {
-		int x, y;
+	for (uint i = 0; i < n; ++i) {
+		long x, y;
 		std::cin >> x >> y;
 		points.emplace_back(K::Point_2(x, y), i);
 	}
 	Delaunay t;
 	t.insert(points.begin(), points.end());
 
+	//Read in bones
+	std::vector<std::multiset<long>> bones_to_tree(n);
+	std::vector<long> distances_bin_search;
+	for (int i = 0; i < m; i++){
+		long x, y; std::cin >> x >> y;
+		K::Point_2 bone = K::Point_2(x, y);
+		auto closest_tree_vertex = t.nearest_vertex(bone);
+		Index closest_tree_index = closest_tree_vertex->info();
+		K::Point_2 closest_tree_point = closest_tree_vertex->point();
+		long squared_dist = CGAL::squared_distance(bone, closest_tree_point)*4;
+		bones_to_tree[closest_tree_index].insert(squared_dist);
+		distances_bin_search.push_back(squared_dist);
+	}
+
+
+	// extract edges and sort by (squared) length
+	// This step takes O(n log n) time (for the sorting).
 	EdgeV edges;
-	std::vector<long> possible_distances_q;
-	graph G(n);
 	edges.reserve(3*n); // there can be no more in a planar graph
 	for (auto e = t.finite_edges_begin(); e != t.finite_edges_end(); ++e) {
 		Index i1 = e->first->vertex((e->second+1)%3)->info();
 		Index i2 = e->first->vertex((e->second+2)%3)->info();
-		K::Point_2 p1 = e->first->vertex((e->second+1)%3)->point();
-		K::Point_2 p2 = e->first->vertex((e->second+2)%3)->point();
 		// ensure smaller index comes first
 		if (i1 > i2) std::swap(i1, i2);
-		edges.emplace_back(i1, i2, t.segment(e).squared_length());
-		if (CGAL::squared_distance(p1, p2) <= s){
-			boost::add_edge(i1, i2, G);
-		}
-		possible_distances_q.push_back(t.segment(e).squared_length());
+		auto sq_length = t.segment(e).squared_length();
+		edges.emplace_back(i1, i2, sq_length);
+		distances_bin_search.push_back(sq_length);
 	}
 	std::sort(edges.begin(), edges.end(),
-		[](const Edge& e1, const Edge& e2) -> bool {
-			return std::get<2>(e1) < std::get<2>(e2);
-			});
+	[](const Edge& e1, const Edge& e2) -> bool {
+		return std::get<2>(e1) < std::get<2>(e2);
+		});
 
-	std::vector<std::pair<int, long>> bones_locations; //Bone i is closest to tree j with dist k: bones_locations[i]=(j,k)
-	std::vector<int> trees_with_bones(n, 0); //Tree i has j bones with smaller r: trees_with_bones[i] = j
-	for (int i = 0; i < m; i++){
-		int x, y; std::cin >> x >> y;
-		K::Point_2 curBone = K::Point_2(x, y);
-
-		auto nearestVertex = t.nearest_vertex(curBone);
-		Index node_nr = nearestVertex->info();
-		K::Point_2 pt = nearestVertex->point();
-		possible_distances_q.push_back(4*CGAL::squared_distance(curBone, pt));
-		//auto dista = CGAL::to_double(CGAL::squared_distance(curBone, pt));
-		bones_locations.push_back(std::make_pair(node_nr, CGAL::squared_distance(curBone, pt)));
-		long radius = s/4;
-		if (CGAL::squared_distance(curBone, pt) <= radius){
-			trees_with_bones[node_nr] += 1;
-		}
-	}
-
+	std::sort(distances_bin_search.begin(), distances_bin_search.end());
 	
-	// reachable bones per tree
-    std::vector<int> bones_count(n);
-    for (auto &bone : bones_locations)
-    {
-        if (bone.second * 4 <= K::FT(s))
-        {
-            bones_count[bone.first]++;
-        }
-    }
-	
-	boost::disjoint_sets_with_storage<> uf(n);
-	Index n_components = n;
-	// ... and process edges in order of increasing length
-	for (EdgeV::const_iterator e = edges.begin(); e != edges.end(); ++e) {
-		// determine components of endpoints
-		Index c1 = uf.find_set(std::get<0>(*e));
-		Index c2 = uf.find_set(std::get<1>(*e));
-		if (c1 != c2) {
-			// this edge connects two different components => part of the emst
-			uf.link(c1, c2);
-			if (std::get<2>(*e) <= s){
-				// edge is not to long -> combine the bone count of both components
-                int combined_bone_count = bones_count[c1] + bones_count[c2];
-                bones_count[c1] = combined_bone_count;
-                bones_count[c2] = combined_bone_count;
-			}
-			if (--n_components == 1) break;
-		}
-	}
-	size_t max_count = 0;
-    for (size_t count : bones_count)
-    {
-        max_count = std::max(max_count, count);
-    }
+	int answer_q1 = nrBones(edges, bones_to_tree, s);
 
-	std::sort(possible_distances_q.begin(), possible_distances_q.end());
-
-	//Do binsearch on the radius
-	long left = 0; long right = possible_distances_q.size()-1;
-	while (left < right){
-		long middle_index = left+(right-left)/2;
-		long middle = possible_distances_q[middle_index];
-
-		int best = nrBones(t, n, middle, bones_locations, edges);
-		if (best >= k){
-			right = middle_index;
+	//Do binsearch on distances array
+	int l = 0; int r = distances_bin_search.size()-1;
+	long smallest_r = std::numeric_limits<long>::max();
+	while (l < r){
+		int mid = l + (r-l)/2;
+		long cur_dist = distances_bin_search[mid];
+		int curNrBones = nrBones(edges, bones_to_tree, cur_dist);
+		if (curNrBones < k){
+			l = mid+1;
 		} else {
-			left = middle_index+1;
+			r = mid;
+			smallest_r = cur_dist;
 		}
-
 	}
+	long answer_q2 = smallest_r;
 
-	std::cout << max_count << " " << possible_distances_q[right] << std::endl;
+	std::cout << answer_q1 << " " << answer_q2 << std::endl;
+
+	return;
 }
 
 int main() {
