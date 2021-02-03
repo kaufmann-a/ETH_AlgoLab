ab80c641fee4e555731cad901694fa830453aadd
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2021-02-03 16:58:03 +0100

    india

--- 12_india/src/algorithm.cpp
+++ 12_india/src/algorithm.cpp
@@ -6,13 +6,12 @@
 #include <algorithm>
 #include <vector>
 #include <fstream>
-// BGL includes
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/cycle_canceling.hpp>
 #include <boost/graph/push_relabel_max_flow.hpp>
 #include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
 #include <boost/graph/find_flow_cost.hpp>
-// Graph Type with nested interior edge properties for Cost Flow Algorithms
+
 typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
     boost::property<boost::edge_capacity_t, long,
@@ -44,7 +43,10 @@ class edge_adder {
   }
 };
 
-std::pair<int, int> calculateCost(int nrElephants, int c, std::vector<std::tuple<int, int, int, int>> gra, int k, int a){
+
+void testcase() {
+	int c, g, b, k, a; std::cin >> c >> g >> b >> k >> a;
+
 	// Create graph, edge adder class and propery maps
     graph G(c+1);
     edge_adder adder(G);  
@@ -52,74 +54,59 @@ std::pair<int, int> calculateCost(int nrElephants, int c, std::vector<std::tuple
     auto r_map = boost::get(boost::edge_reverse, G);
     auto rc_map = boost::get(boost::edge_residual_capacity, G);
 
-	int source = c; 
-	adder.add_edge(source, k, nrElephants, 0);
+	int start_node = c;
 
-	for (int i = 0; i < gra.size(); i++){
-		int from = std::get<0>(gra[i]);
-		int to = std::get<1>(gra[i]);
-		int cost = std::get<2>(gra[i]);
-		int cap = std::get<3>(gra[i]);
-		adder.add_edge(from, to, cap, cost);
-	}
-
-	// Option 2: Min Cost Max Flow with successive_shortest_path_nonnegative_weights  
-    boost::successive_shortest_path_nonnegative_weights(G, source, a);
-    int cost2 = boost::find_flow_cost(G);
-    int s_flow = 0;
-    out_edge_it e, eend;
-    for(boost::tie(e, eend) = boost::out_edges(boost::vertex(source,G), G); e != eend; ++e) {
-        s_flow += c_map[*e] - rc_map[*e];     
-    }
-	return std::make_pair(cost2, s_flow);
-}
-
-void testcase() {
-	int c, g, b, k, a; std::cin >> c >> g >> b >> k >> a;
-
-	std::vector<std::tuple<int, int, int, int>> G(g);
+	//read in edges
 	for (int i = 0; i < g; i++){
 		int x, y, d, e; std::cin >> x >> y >> d >> e;
-		G[i] = std::make_tuple(x, y, d, e);
+		adder.add_edge(x, y, e, d);
 	}
 
-	//Do binary search on the number of elephants
-	//Max nr. elephants = max_e*nrGuides* = 10³*5*10³
-	int l = 0; 
-	int r = 5000000;
-	int curBest = 0;
-	while (l <= r){
-		int curNrElephants = l+(r-l)/2;
-		std::pair<int, int> result = calculateCost(curNrElephants, c, G, k, a);
+	//Add edge start node
+	adder.add_edge(start_node, k, std::numeric_limits<int>::max(), 0);
+
+	boost::successive_shortest_path_nonnegative_weights(G, start_node, a);
+    int cost2 = boost::find_flow_cost(G);
+
+	int s_flow = 0;
+    out_edge_it e, eend;
+    for(boost::tie(e, eend) = boost::out_edges(boost::vertex(start_node,G), G); e != eend; ++e) {
+        s_flow += c_map[*e] - rc_map[*e];     
+    }
+
+	//Maybe already in budget?
+	if (cost2 <= b){
+		std::cout << s_flow << std::endl;
+		return;
+	}
+
+	//Do binsearch 
+	int l = 0; int r = s_flow; 
+	int cur_best = 0;
+	while (l < r){
+		int mid = l + (r-l)/2;
+
+		out_edge_it ebg, eend;
+		for (boost::tie(ebg, eend) = boost::out_edges(c, G); ebg != eend; ++ebg){
+			int cur_cap = boost::get(boost::edge_capacity_t(), G, *ebg);
+			boost::put(boost::edge_capacity_t(), G, *ebg, mid);
+		}
+		boost::successive_shortest_path_nonnegative_weights(G, start_node, a);
+    	int next_cost = boost::find_flow_cost(G);
 		
-		//First check if enough elephands available
-		if (result.second < curNrElephants){
-			//If not enough elephants, check if in budged with used elephants
-			if (result.first < b){
-				curBest = result.second;
-				break;
-			} else {
-				r = result.second;
-			}
+		if (next_cost <= b){
+			cur_best = mid;
+			l = mid+1;
 		} else {
-			if (result.first < b) {
-				l = curNrElephants+1;
-				curBest = curNrElephants;
-			} else if (result.first > b) {
-				r = curNrElephants-1;
-			} else if (result.first == b){
-				curBest = curNrElephants;
-				break;
-			}
+			r = mid;
 		}
 	}
-	std::cout << curBest << std::endl;
-	return;
+	std::cout << cur_best << std::endl;
 }
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
-	std::fstream in("./testsets/test1.in");
+	std::fstream in("./testsets/sample.in");
 	std::cin.rdbuf(in.rdbuf());
 
 	int t;
